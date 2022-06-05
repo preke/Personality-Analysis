b498f5a4789519a9650b1a111f68bf0c8ebf730a
preke <geek00021@gmail.com>
2022-06-05 23:51:10 +0800

    .

--- src/model.py
+++ src/model.py
@@ -262,7 +262,7 @@ class Scaled_Dot_Product_Attention(nn.Module):
         print(mask)
         print(mask.shape)
 
-        mask = mask.unsqeeze(-1).expand(-1, -1, attention.shape[1])
+        mask = mask.unsqueeze(-1).expand(-1, -1, attention.shape[1])
         print(mask.shape)
         print(mask)
         attention = attention * scale
