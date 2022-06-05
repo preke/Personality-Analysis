d1e0189d9d1ff96894e9e022635bdb649a8f692e
preke <geek00021@gmail.com>
2022-06-05 17:38:19 +0800

    .

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from transformers import RobertaConfig, RobertaModel, RobertaTokenizer, RobertaF
 parser = argparse.ArgumentParser(description='')
 args   = parser.parse_args()
 
-args.device       = 1
+args.device       = 0
 
 args.SEED          = 42
 args.MAX_LEN       = 32
