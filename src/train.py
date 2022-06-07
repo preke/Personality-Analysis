8b346c31194d273dcc2e84bb53de76f01ad6e220
preke <geek00021@gmail.com>
2022-06-07 10:58:13 +0800

    .

--- src/train.py
+++ src/train.py
@@ -22,11 +22,11 @@ def train_model(model, args, train_dataloader, valid_dataloader, train_length):
     num_warmup_steps   = int(0*train_length) # first 1 epoch for warm-up
     num_training_steps = len(train_dataloader)*args.epochs
     
-    for name, param in model.named_parameters():        
-        if name.startswith('bert'):
-            param.requires_grad = False
-        else:
-            print(name,param.size())
+    # for name, param in model.named_parameters():        
+    #     if name.startswith('bert'):
+    #         param.requires_grad = False
+    #     else:
+    #         print(name,param.size())
     
 
     
