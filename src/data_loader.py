82ae4577b09a408e8ef003bc04cc95da1bfedab2
preke <geek00021@gmail.com>
2022-06-09 17:29:41 +0800

    .

--- src/data_loader.py
+++ src/data_loader.py
@@ -27,9 +27,8 @@ def get_vad(VAD_dict, sents):
                 v_score.append(0)
                 a_score.append(0)
                 d_score.append(0)
-            print(word, v_score, a_score, d_score)
         VAD_scores.append([v_score, a_score, d_score])
-        print()
+        print(sent, VAD_scores)
     import time
     time.sleep(100)
     return VAD_scores
