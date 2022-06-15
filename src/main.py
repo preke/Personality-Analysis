88ad64f928d0be0255a15c0c252405269d05a490
preke <geek00021@gmail.com>
2022-06-16 04:20:04 +0800

    .

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ args.d_transformer = 128
 
 
 args.mode         = 'Uttr' #Context_Hierarchical_emoberta_uttr' #_emoberta_uttr'
-args.BASE         = 'RoBERTa'
+args.BASE         = 'BERT'
 args.VAD_tokenized_dict = '../VAD_tokenized_dict.json'
 args.result_name  = args.mode + '.txt' 
 
