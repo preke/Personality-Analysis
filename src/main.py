b560a6006bc6a488b9ea2303d9477b085658e142
preke <geek00021@gmail.com>
2022-06-16 12:02:17 +0800

    .

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='')
 args   = parser.parse_args()
 
 args.device        = 1
-args.MAX_LEN       = 128
+args.MAX_LEN       = 256
 args.MAX_NUM_UTTR  = 40
 args.batch_size    = 32
 args.adam_epsilon  = 1e-8
@@ -46,7 +46,7 @@ for r in VAD_Lexicons.iterrows():
 args.VAD_dict = VAD_dict
 
 
-personalities = ['O']
+personalities = ['A','C','E','O','N']
 
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
