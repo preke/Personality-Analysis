515216a88c5eb0d2126751299eeb1c387df86fcb
preke <geek00021@gmail.com>
2022-06-16 18:16:41 +0800

    .

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ args.test_size     = 0.1
 args.d_transformer = 128
 
 
-args.mode         = 'Context' #_Hierarchical_emoberta_uttr' #_emoberta_uttr'
+args.mode         = 'Full_dialog' #_Hierarchical_emoberta_uttr' #_emoberta_uttr'
 args.BASE         = 'RoBERTa'
 args.VAD_tokenized_dict = '../VAD_tokenized_dict.json'
 args.result_name  = args.mode + '.txt' 
