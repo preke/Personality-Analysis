34e8537cc229a6f14f41e06ccda9ad57ca54dcb8
preke <geek00021@gmail.com>
2022-06-16 16:17:28 +0800

    .

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='')
 args   = parser.parse_args()
 
 args.device        = 1
-args.MAX_LEN       = 256
+args.MAX_LEN       = 2128
 args.MAX_NUM_UTTR  = 40
 args.batch_size    = 32
 args.adam_epsilon  = 1e-8
@@ -53,7 +53,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 if args.BASE == 'BERT':
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
-    epoch_list = [3]
+    epoch_list = [4]
     lr_list = [1e-5]
 elif args.BASE == 'RoBERTa':
     tokenizer = RobertaTokenizer.from_pretrained("roberta-base", do_lower_case=True)
