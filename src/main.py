7b992af5a2d891d1eb0ee3c5347d98abaa4141d1
preke <geek00021@gmail.com>
2022-06-16 09:05:24 +0800

    .

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ for r in VAD_Lexicons.iterrows():
 args.VAD_dict = VAD_dict
 
 
-personalities = ['A']
+personalities = ['C']
 
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
