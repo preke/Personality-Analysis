fb7beafdb74d231c0923a00a4894494f216d6e46
preke <geek00021@gmail.com>
2022-06-18 23:28:33 +0800

    .

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ args.test_size     = 0.1
 args.d_transformer = 128
 
 
-args.mode         = 'Context_Hierarchical' #'Uttr'#Full_dialog' #_emoberta_uttr'
+args.mode         = 'Context_Hierarchical_affective' #'Uttr'#Full_dialog' #_emoberta_uttr'
 args.BASE         = 'BERT'
 args.VAD_tokenized_dict = '../VAD_tokenized_dict.json'
 args.result_name  = args.mode + '.txt' 
