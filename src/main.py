51bd816ba19ed563311f3940bd1100065d562096
preke <geek00021@gmail.com>
2022-06-21 14:05:15 +0800

    .

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 if args.BASE == 'BERT':
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
-    epoch_list = [4]
+    epoch_list = [3]
     lr_list = [2e-5]
 elif args.BASE == 'RoBERTa':
     tokenizer = RobertaTokenizer.from_pretrained("roberta-base", do_lower_case=True)
