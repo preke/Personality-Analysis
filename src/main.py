6fcc13b6c9b0e3509d05ea38dba26eeb7c2c6f3d
preke <geek00021@gmail.com>
2022-06-23 11:17:51 +0800

    .

--- src/main.py
+++ src/main.py
@@ -62,8 +62,8 @@ if args.BASE == 'BERT':
     lr_list = [5e-5]
 elif args.BASE == 'RoBERTa':
     tokenizer = RobertaTokenizer.from_pretrained("roberta-base", do_lower_case=True)
-    epoch_list = [4]
-    lr_list = [1e-3]
+    epoch_list = [8]
+    lr_list = [2e-4]
 
 args.lr = lr_list[0]
 
