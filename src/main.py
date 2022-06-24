32b647ae94ecd87073a303e813a3dca4a72c48e6
preke <geek00021@gmail.com>
2022-06-24 14:24:37 +0800

    .

--- src/main.py
+++ src/main.py
@@ -77,7 +77,7 @@ if args.data == 'Friends_Persona':
     args.MAX_NUM_UTTR  = 40
 else:
     personalities = ['Chandler', 'Joey','Rachel','Monica','Phoebe','Ross'] # ,
-    args.batch_size    = 256
+    args.batch_size    = 128
     args.MAX_NUM_UTTR  = 3
 
 with open(args.result_name, 'w') as f:
